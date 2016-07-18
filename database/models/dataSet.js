824951a493cdac7856ee911ca233eb67a075c3a8
Karen Zapata <kzapata0919@gmail.com>
2016-07-18 13:21:05 -0700

    feat - Implement user and dataset models

--- database/models/dataSet.js
+++ database/models/dataSet.js
@@ -1,3 +1,13 @@
 //The datasets collection will tentatively consist of documents with time stamps:
 //chart: object
 //username: string
+var mongoose = require('mongoose');
+
+var dataSetSchema = mongoose.Schema({
+  chart: Object,
+  username: String
+});
+
+var DataSet = mongoose.model('DataSet', dataSetSchema);
+
+module.exports = DataSet;
