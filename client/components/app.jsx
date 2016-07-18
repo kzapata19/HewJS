6894865cbf4e53d244773acff8cccae7e98ed39a
Peranat Dayananda <peranatdayananda@gmail.com>
2016-07-19 05:47:30 +0700

    feat/input add prototype file drop zone

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,16 +1,29 @@
+const Input = require('./input.jsx');
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      message: 'HewJS in React'
-    }
+      message: 'HewJS in React',
+      input: 'this is the data from the input field'
+    };
+  }
+
+  handleInput(files) {
+    console.log('DROPPED');
+    console.log(files);
   }
 
   render() {
     return (
       <div>
-        <h1>{this.state.message}</h1>
+        <div>
+          <h1>{this.state.message}</h1>
+        </div>
+        <div>
+          <Input input={this.state.input} handler={this.handleInput} />
+        </div>
       </div>
     );
   }
--- client/components/input.jsx
+++ client/components/input.jsx
@@ -1,7 +1,15 @@
+const Dropzone = require('react-dropzone');
+
 const Input = (props) => {
 
   return (
     <div>
+     <Dropzone onDrop={props.handler}>
+         <div>Drop some files here!</div>
+     </Dropzone>
+      <p>{props.input}</p>
     </div>
   );
 };
+
+module.exports = Input;
