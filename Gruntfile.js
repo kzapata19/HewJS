01a919171a8a8667756d7b94aec6ee91e8b672b1
Ron Cruz <Ronolibert@gmail.com>
2016-07-17 22:11:53 -0700

    (chore) Add grunt rebase command

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,13 +18,22 @@ module.exports = function(grunt) {
         src: ['client/components/app.jsx'],
         dest: 'client/compiled/components/app.js',
       }
+    },
+    shell: {
+      command: 'git pull --rebase upstream master'
     }
 
   });
-
+  grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browserify');
 
   grunt.registerTask('default', ['browserify']);
+
+
+
+  grunt.registerTask('rebase', function() {
+    grunt.task.run(['shell']);
+  })
 
 };
