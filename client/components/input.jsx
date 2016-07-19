98be55c4c9dbafbc7edbdc705f574e600035a0c0
Peranat Dayananda <peranatdayananda@gmail.com>
2016-07-20 04:01:05 +0700

    feat - add text area inside dropzone to handle text input, disable on click upload prompt

--- client/components/input.jsx
+++ client/components/input.jsx
@@ -2,11 +2,14 @@ const Dropzone = require('react-dropzone');
 
 const Input = (props) => {
 
+  let liveReload;
+
   const handleInput = (files) => {
     const context = props.context;
     const file = files[0];
     const read = new FileReader();
     read.onload = function(event) {
+      $('#textArea').val(event.target.result);
       context.setState({
         input: event.target.result.split('\n').map(line => line.split(','))
       });
@@ -14,12 +17,29 @@ const Input = (props) => {
     read.readAsText(file);
   }
 
+  const handleText = () => {
+    let input = $('#textArea').val();
+    if (input) {
+      input = input.split('\n').map(line => line.split(','));
+      props.context.setState({
+        input: input
+      });
+    }
+  }
+
+  const liveText = () => {
+    clearTimeout(liveReload);
+    liveReload = setTimeout(function() {
+      handleText();
+    }, 500);
+  }
+
   return (
     <div>
-     <Dropzone onDrop={handleInput}>
-         <div>Drop a file here!</div>
-     </Dropzone>
-     <p>{JSON.stringify(props.context.state.input)}</p>
+      <Dropzone id="dropzone" onDrop={handleInput} disableClick={true}>
+        <textarea id="textArea" onChange={liveText} placeholder="paste or drop your CSV here!"></textarea>
+      </Dropzone>
+      <p>{JSON.stringify(props.context.state.input)}</p>
     </div>
   );
 };
