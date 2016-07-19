dacd77a04ae2478b4058cdf5f82c14c3565ee0e6
Jamie Long <jtlong3rd@gmail.com>
2016-07-18 21:51:12 -0400

    feat: Add PUT and DELETE routes

--- routes/routeHelpers.js
+++ routes/routeHelpers.js
@@ -1,7 +1,10 @@
+let _ = require('underscore');
+
 let db = require('../database/config');
 
 let User = require('../database/models/user');
 let DataSet = require('../database/models/dataSet');
+
 
 exports.getAllUsers = function(callback) {
   User.find({}, {'_id': 0, 'username': 1, 'password': 1}, function(err, users) {
@@ -59,6 +62,46 @@ exports.addDataSet = function(dataSet, callback) {
       callback(err, null);
     } else {
       callback(null, result);
+    }
+  });
+};
+
+exports.updateUser = function(username, newProperties, callback) {
+  User.findOneAndUpdate({'username': username}, newProperties, function(err, user) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, user);
+    }
+  });
+};
+
+exports.updateDataSet = function(chartName, newProperties, callback) {
+  DataSet.findOneAndUpdate({'chartName': chartName}, newProperties, function(err, dataSet) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, dataSet);
+    }
+  });
+};
+
+exports.deleteUser = function(username, callback) {
+  User.remove({'username': username}, function(err, result) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, username);
+    }
+  });
+};
+
+exports.deleteChart = function(chartName, callback) {
+  DataSet.remove({'chartName': chartName}, function(err, result) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, chartName);
     }
   });
 };
