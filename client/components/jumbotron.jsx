0fdee63d78022f4d15b96c514e98228ef2651f07
Ron Cruz <Ronolibert@gmail.com>
2016-07-22 10:38:13 -0700

    (refactor) Add components for html5 tags

---
+++ client/components/jumbotron.jsx
@@ -0,0 +1,20 @@
+const Jumbotron = () => {
+  return (
+    <div id="particles-js" className="jumbotron">
+      <div className="container">
+        <div className="navbar-header">
+          <a href="#">HewJS</a>
+        </div>
+        <div className="navbar-collapse collapse">
+          <ul className="nav navbar-nav navbar-right">
+            <li><a href="">Home</a></li>
+            <li><a href="#team-section" className="team ">Team</a></li>
+            <li><a href="https://github.com/bulbasaur-HRR17/HewJS">Github</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+module.exports = Jumbotron;
