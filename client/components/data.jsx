413ab2368998453f8cedd03dc3a2cee9f845c637
Peranat Dayananda <peranatdayananda@gmail.com>
2016-07-22 07:49:19 +0700

    refactor - move formatting functions into Chart component feat - add dropdown for chart types

--- client/components/data.jsx
+++ client/components/data.jsx
@@ -6,11 +6,11 @@ import React from 'react';
 
 // Chart's required format
 // {
-//   data: [{ "key": value, "key2": value}],
-//   xAxis: "",
+//   dataset: [{ "key": value, "key2": value}],
+//   x: "",
 //   charts: [
 //     {
-//       yAxis: "key",
+//       y: "key",
 //       type: "type"
 //     }
 //   ]
@@ -38,80 +38,4 @@ const Data = (props) => {
 
 }
 
-// class Data extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     console.log(this.props.rawData);
-//     this.state = {
-//       xAxis: [],
-//       yAxis: []
-//     };
-//   }
-
-//   formatChartForProp() {
-//     let result = [];
-
-//   }
-
-//   formatDataForProp(input) {
-//     const result = [];
-//     const keys = Object.keys(input);
-//     for (let i = 0; i < input[keys[0]].length; i++) {
-//       let entry = {};
-//       for (let key of keys) {
-//         entry[key] = input[key][i];
-//       }
-//       result.push(entry);
-//     }
-//     return result;
-//   }
-
-//   formatPropForChart() {
-//     const chartProp = {};
-//     chartProp.data = this.formatDataForProp(this.props.rawData);
-//     chartProp.xAxis = this.props.app.xAxis;
-//     chartProp.charts = [];
-//     return chartProp;
-//   }
-
-//   setAxes(e) {
-//     if (e.target.value !== "---choose-a-value---") {
-//       let axis = {};
-//       axis[e.target.dataset.axis] = e.target.value;
-//       this.setState(axis);
-//     }
-//   }
-
-//   showIt() {
-//     console.log(this.formatPropForChart());
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <div>
-//         {
-//           this.props.choosers.map(axis =>
-//             <select onChange={this.props.setAxes} data-axis={axis}>
-//               <option value="---choose-a-value---">choose a value</option>
-//               {
-//                 Object.keys(this.props.rawData).map(key =>
-//                 <option value={key}>{key}</option>)
-//               }
-//             </select>
-//           )
-//         }
-//           <button onClick={this.showIt.bind(this)}>Show it</button>
-//         </div>
-//         <div>
-//           <Chart data={this.props.app} type={this.props.choice}/>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 module.exports = Data;
-
-
-// {day: [mon, tues, wed, thur, fri, sat, sun], hours: [1,2,3,4,5,6,7] }
