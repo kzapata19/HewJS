e4e0e145975e33510da75a340bb2899153509cf8
Jamie Long <jtlong3rd@gmail.com>
2016-07-23 22:51:49 -0400

    feat: Add helper functions for making the AJAX requests associated with signup/login/logout

--- client/ajaxRequests.js
+++ client/ajaxRequests.js
@@ -1,8 +1,38 @@
 import $ from 'jquery';
 
-const loadSet = function(username, chartName, callback) {
+const signup = function(username, password, callback) {
   return $.ajax({
-    url: `/api/datasets/${username}/${chartName}`
+    url: '/signup',
+    method: 'POST',
+    data: {
+      'username': username,
+      'password': password
+    }
+  }).done(function(result){
+    callback(null, result);
+  }).error(function(err){
+    callback(err, null);
+  });
+};
+
+const login = function(username, password, callback) {
+  return $.ajax({
+    url: '/login',
+    method: 'POST',
+    data: {
+      'username': username,
+      'password': password
+    }
+  }).done(function(result){
+    callback(null, result);
+  }).error(function(err){
+    callback(err, null);
+  });
+};
+
+const logout = function(callback) {
+  return $.ajax({
+    url: '/logout',
     method: 'GET'
   }).done(function(data){
     callback(null, data);
@@ -11,18 +41,31 @@ const loadSet = function(username, chartName, callback) {
   });
 };
 
-const saveSet = function(username, dataSet, chartName, callback) {
+const saveDataSet = function(username, dataSet, chartName, callback) {
   return $.ajax({
-    url: '/api/datasets/'
+    url: '/api/datasets/',
     method: 'POST',
     data: {
       'username': username,
       'dataSet': dataSet,
       'chartName': chartName
     }
-  }).done(function(result){
+  }).done(function(result) {
     callback(null, result);
+  }).error(function(err) {
+    callback(err, null);
+  });
+};
+
+const loadDataSet = function(username, chartName, callback) {
+  return $.ajax({
+    url: `/api/datasets/${username}/${chartName}`,
+    method: 'GET'
+  }).done(function(data){
+    callback(null, data);
   }).error(function(err){
     callback(err, null);
   });
 };
+
+module.exports = { signup, login, logout, saveDataSet, loadDataSet };
